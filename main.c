72fb205b47e66a6d7e5a25b5281ba9a948494ec2
manjitsinghduhan <duhan.manjit@gmail.com>
2017-03-29 00:38:36 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+
+/* ************************** Inclusion directives ************************** */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "fibonacci.h"
+
+
+
+/******************************************************************************
+* Function Name:  main
+**************************************************************************//**
+*   @brief      main function
+*   @param[in]  None
+*   @param[out] None
+*   @retval     None
+*   @warning    None
+*   @remark     None
+*//***************************************************************************/
+
+
+
+int main(void)
+{
+	int num = 0;   //declared a number n for f(n)
+
+	setbuf(stdout, NULL);
+
+	printf("Enter the value of n in f(n) : ");
+	scanf("%d", &num);
+
+	/*check that the entered number is positive. If not ask again to enter the number*/
+	while(num < 0)
+	{
+		printf("\nTry again with positive number");
+		printf("\nEnter the value of n in f(n) : ");
+		scanf("%d", &num);
+	}
+
+	fibonacci(num);  // tempValue = f(n)
+
+	return 0;
+}
